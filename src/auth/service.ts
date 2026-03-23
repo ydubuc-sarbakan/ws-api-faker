b1a02632c45bae0d93f19eba68efcd524097134f
Yoan <yoan.dubuc@sarbakan.com>
2026-03-23 15:25:40 -0400

    random qr code

--- src/auth/service.ts
+++ src/auth/service.ts
@@ -1,8 +1,5 @@
 export class AuthService {
     async generateQrCodeUrl(machineId: string): Promise<string> {
-        return (
-            'https://upload.wikimedia.org/wikipedia/commons/3/31/MM_QRcode.png?machineId=' +
-            encodeURIComponent(machineId)
-        );
+        return `https://api.qrserver.com/v1/create-qr-code/?size=768x768&data=http://192.168.7.57:9002?machineId=${encodeURIComponent(machineId)}`;
     }
 }
